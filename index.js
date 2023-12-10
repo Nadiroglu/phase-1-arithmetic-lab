3015d7ad02ee6a1195129b8388ad5f97039ea362
Nail Nadiroglu <nadiroglunail@gmail.com>
2023-12-09 20:36:01 -0500

    max,ceil,floor,multiply,remainder

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+let a = random;
+let random = Math.floor(Math.random() * 10);
+let num3 = 24
+let num4 = 5
+const mod = num3 % num4
 
-// Write your code here
+let max = Math.max(1,2,14,15,20)
+
